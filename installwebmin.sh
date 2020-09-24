347ca1a68fd7e94c13dce20bd833b2ac64137ba5
merlinjk <60071741+merlinjk@users.noreply.github.com>
2020-09-24 11:38:18 +0200

    Update installwebmin.sh

--- installwebmin.sh
+++ installwebmin.sh
@@ -3,7 +3,7 @@
 
 #!/bin/bash
 
-sudo echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/so$
+sudo echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
 sudo apt update
 sudo apt install webmin -y
